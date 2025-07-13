ff2b6720ea239bd883b19c932eadb43b9dac7763
pras <prashantsah567@gmail.com>
2025-07-13 01:07:19 -0600

    logic added to upload output image to s3

--- backend/app/api/generate_runpod.py
+++ backend/app/api/generate_runpod.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.services.runpod import generate_image
+from app.utils.s3 import upload_file_to_s3
 
 #exposing runpod image generator function via Fast API (to call from frontend)
 
@@ -15,6 +16,8 @@ def generate_endpoint(request: PromptRequest):
         print(f"Received prompt: {request.prompt}")
         output_file = generate_image(request.prompt)
         print(f"Generated image saved to : {output_file}")
+        image_url = upload_file_to_s3(output_file)
+        print("Image URL:",image_url)
         return {"status": "success", "file": output_file}
     except Exception as e:
         print(f"Error during generation: {str(e)}")
