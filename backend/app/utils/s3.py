c4e103c3dcebb4988ef591636320141d650e014b
pras <prashantsah567@gmail.com>
2025-07-13 01:59:58 -0600

    trying to read .env

--- backend/app/utils/s3.py
+++ backend/app/utils/s3.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv("backend/.env")
 
 import boto3
 import os
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv("backend/.env")
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
